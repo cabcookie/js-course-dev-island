5122870fe01dee15338e0168fb892fdffcc4f4fe
Carsten Koch <carstenkoch@Carstens-MacBook-Pro.local>
2018-05-18 01:18:37 +0200

    Experimenting with Booleans, undefined and null

--- s2c8-variables/code.js
+++ s2c8-variables/code.js
@@ -1,21 +1,76 @@
-// Strings
-var job = 'web developer';
-var town = "London";
+console.warn('if you compare type javascript does adapt the types');
+console.warn("5 == '5'");
+var x = 5;
+var y = '5';
+var z = x == y;
+console.log(z);
 
-var greeting = 'Hello, I\'m glad to see you';
-var quote = "This is \n a new line";
-console.log(quote);
+console.warn("This doesn't work if you compare with === because then the types are not adapted");
+console.warn("5 === '5'");
+z = x === y;
+console.log(z);
 
-var tab = "Here is \t a tab.";
-console.log(tab);
+console.warn("Logical operators");
+console.warn("z = (3 < 5) && (3 > 0)");
+x = 3;
+y = 5;
+z = (x < y) && (x > 0);
+console.log(z);
 
-var first = 'Bill';
-var last = 'Gates';
-var fullname = first + ' ' + last;
-console.log(fullname);
+console.warn("Operator precedence");
+console.warn("&& is stronger than ||");
+console.warn("false && true || true");
+z = false && true || true; // && stronger than ||
+console.log(z);
 
-console.log(fullname.length);
-console.log(fullname.charAt(5));
-console.log(fullname.indexOf('B'));
-console.log(fullname.indexOf('G'));
-console.log(fullname.lastIndexOf('l'));
+console.warn("Unary Operator");
+console.warn("x > 4 ? 'Pass' : 'Fail'");
+z = x > 4 ? 'Pass' : 'Fail';
+console.log(z);
+console.warn("x > 2 ? 'Pass' : 'Fail'");
+z = x > 2 ? 'Pass' : 'Fail';
+console.log(z);
+
+console.warn("Logic operators with non Booleans");
+console.warn("With && if the left side doesn't cohere to false, the right side is the result");
+console.warn("true && 'apple'");
+z = true && 'apple';
+console.log(z);
+console.warn("1 && 'apple'");
+z = 1 && 'apple';
+console.log(z);
+console.warn("'1' && 'apple'");
+z = '1' && 'apple';
+console.log(z);
+console.warn("false && 'apple'");
+z = false && 'apple';
+console.log(z);
+console.warn("'banana' && 'apple'");
+z = 'banana' && 'apple';
+console.log(z);
+
+console.warn("With || if the left side cohere to false, the right side is the result");
+console.warn("undefined || 'apple'");
+var fruit;
+z = fruit || 'apple';
+console.log(z);
+console.warn("'' || 'apple'");
+fruit = '';
+z = fruit || 'apple';
+console.log(z);
+console.warn("'banana' || 'apple'");
+fruit = 'banana'
+z = fruit || 'apple';
+console.log(z);
+
+console.warn("null and undefined");
+var i;
+console.warn("undefined");
+console.log(i);
+console.warn("typeof undefined");
+console.log(typeof i);
+i = null;
+console.warn("null");
+console.log(i);
+console.warn("typeof null");
+console.log(typeof i);
