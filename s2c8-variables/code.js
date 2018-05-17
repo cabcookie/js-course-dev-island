02a445fc26334e72f4f1311d44d36f56571bb936
Carsten Koch <carstenkoch@Carstens-MacBook-Pro.local>
2018-05-17 12:27:47 +0200

    Basic variables and data types

---
+++ s2c8-variables/code.js
@@ -0,0 +1,47 @@
+var score;
+console.log(score);
+// undefined is a value or more precise a type
+
+var livesLeft = 3;
+console.log(livesLeft);
+livesLeft = 2;
+console.log(livesLeft);
+console.log(typeof livesLeft);
+
+const FIRSTNAME = 'John';
+console.log(FIRSTNAME);
+// FIRSTNAME = 'Max'; this fails
+
+var x = 5;
+var y = 2.4;
+console.log(typeof y);
+console.log(y);
+
+var z = 5.3e8
+console.log(typeof z);
+
+var min = Number.MIN_VALUE;
+var max = Number.MAX_VALUE;
+console.log(min);
+console.log(max);
+
+var a = 4 + 9;
+console.log(a);
+
+a = 3 * 4;
+console.log(a);
+
+a = y - x;
+console.log(a);
+
+var b = 3;
+b += 2;
+console.log(b);
+
+b++;
+console.log(b);
+
+b--;
+console.log(b);
+
+console.log(12 / 4);
